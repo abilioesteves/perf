a70a1249ccf88d76949612dd1c2f89437d657917
Abilio Esteves <abilio.esteves@ibm.com>
2019-01-10 17:04:22 -0200

    added compose with prometheus; fixed contenty type

--- src/hook/default.go
+++ src/hook/default.go
@@ -1,6 +1,7 @@
 package hook
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
@@ -24,7 +25,7 @@ func NewDefaultHook(perfChecker perf.Checker) *DefaultHook {
 		perfChecker: perfChecker,
 	}
 
-	hook.router.HandleFunc("/diskperf", hook.DiskPerf).Methods("GET")
+	hook.router.HandleFunc("/diskperf/{format}", hook.DiskPerf).Methods("GET")
 
 	return hook
 }
@@ -43,9 +44,32 @@ func (hook *DefaultHook) Init() {
 // DiskPerf Gets the read and write performance of the disk
 func (hook *DefaultHook) DiskPerf(w http.ResponseWriter, r *http.Request) {
 	defer gohserver.HandleError(w)
+	format := "json"
+
+	vars := mux.Vars(r)
+	if len(vars) > 0 {
+		format = vars["format"]
+	}
 
 	info, err := hook.perfChecker.DiskPerf()
 	gohtypes.PanicIfError("Not possible to check disk performance", 500, err)
 
-	gohserver.WriteJSONResponse(info, 200, w)
+	if format == "prom" {
+		writePrometheusResponse(info, 200, w)
+	} else {
+		gohserver.WriteJSONResponse(info, 200, w)
+	}
+}
+
+func writePrometheusResponse(info perf.DiskPerfInfo, status int, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(status)
+
+	response := fmt.Sprintln("# HELP disk_write_perf A gauge for the disk performance")
+	response = response + fmt.Sprintln("# TYPE disk_write_perf gauge")
+	response = response + fmt.Sprintf("disk_write_perf{unit=\"%v\"} %v", info.Unit, info.WriteSpeed)
+	_, err := w.Write([]byte(response))
+	gohtypes.PanicIfError(fmt.Sprintf("Not possible to write %v response", status), 500, err)
+
+	logrus.Infof("200 Response sent. Payload: %s", info)
 }
